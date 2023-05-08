388485b207e3f9616ad51b5ddc36a6a64831a0c7
muhammedyasirm <87080246+muhammedyasirm@users.noreply.github.com>
2023-05-08 13:06:18 +0530

    final

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
     })
 } else {
-    app.get("/",(req,res) => {
+    app.get("/backend",(req,res) => {
         res.send("API is running successfully");
     });
 }
