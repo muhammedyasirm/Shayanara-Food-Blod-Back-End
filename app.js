76ad27e8398e984903c505fb689af8c0a60bc688
muhammedyasirm <87080246+muhammedyasirm@users.noreply.github.com>
2023-05-08 11:59:40 +0530

    final

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const postRouter = require('./routes/postRoutes');
 const adminRouter = require('./routes/adminRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require('./routes/messageRoutes');
+const path = require('path');
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,6 +20,22 @@ app.use('/backend',postRouter);
 app.use('/backend/admin',adminRouter);
 app.use('/backend/chat',chatRoutes);
 app.use('/backend/message',messageRoutes);
+
+//-----------------Deployment---------------------
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1,"/frontend/buid")))
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+    })
+} else {
+    app.get("/",(req,res) => {
+        res.send("API is running successfully");
+    });
+}
+
+//-----------------Deployment----------------------
 
 
 mongoose.connect('mongodb://localhost:27017/shayanara',{
