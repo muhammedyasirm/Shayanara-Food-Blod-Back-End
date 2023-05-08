a5c3ec260712f9b532fdc8a19a592fcd40ec7a8d
muhammedyasirm <87080246+muhammedyasirm@users.noreply.github.com>
2023-05-08 15:20:49 +0530

    final

--- app.js
+++ app.js
@@ -16,11 +16,17 @@ require("dotenv").config();
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use('/backend',userRouter);
-app.use('/backend',postRouter);
-app.use('/backend/admin',adminRouter);
-app.use('/backend/chat',chatRoutes);
-app.use('/backend/message',messageRoutes);
+// app.use('/backend',userRouter);
+// app.use('/backend',postRouter);
+// app.use('/backend/admin',adminRouter);
+// app.use('/backend/chat',chatRoutes);
+// app.use('/backend/message',messageRoutes);
+
+app.use('/',userRouter);
+app.use('/',postRouter);
+app.use('/admin',adminRouter);
+app.use('/chat',chatRoutes);
+app.use('/message',messageRoutes);
 
 //-----------------Deployment---------------------
 
@@ -50,6 +56,7 @@ const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
         origin: "https://shayanara.online"
+        //origin: "http://localhost:3000"
     }
 });
 
